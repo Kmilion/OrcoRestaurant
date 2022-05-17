92d1eb7056034f3d7864e5a65e478eeb7cab30dc
Gonzalo Ceballos <gonzalo.ceballos@valtech.com>
2022-05-16 21:20:49 -0300

    S10-CouponAPIService

---
+++ Orco.Services.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orco.Services.CouponAPI.Models.DTOs;
+using Orco.Services.CouponAPI.Repository;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Orco.Services.CouponAPI.Controllers
+{
+    [ApiController]
+    [Route("api/coupon")]
+    public class CouponController : Controller
+    {
+        private readonly ICouponRepository _couponRepository;
+        protected ResponseDTO _response;
+
+        public CouponController(ICouponRepository couponRepository)
+        {
+            _couponRepository = couponRepository;
+            _response = new ResponseDTO();
+        }
+
+        [HttpGet("{couponCode}")]
+        public async Task<object> GetDiscountForCode(string couponCode)
+        {
+            try
+            {
+                CouponDTO couponDTO = await _couponRepository.GetCouponByCode(couponCode);
+                _response.Result = couponDTO;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+    }
+}
---
+++ Orco.Services.CouponAPI/Repository/ICouponRepository.cs
@@ -0,0 +1,10 @@
+﻿using Orco.Services.CouponAPI.Models.DTOs;
+using System.Threading.Tasks;
+
+namespace Orco.Services.CouponAPI.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<CouponDTO> GetCouponByCode(string couponCode);
+    }
+}
